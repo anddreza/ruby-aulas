df064fa2bddae85942aa7b36f868ba69c9e0e672
Andreza Silva <dedezasilva@live.com>
2021-06-18 16:37:21 -0400

    controle de fluxo com ruby

---
+++ carteira.rb
@@ -0,0 +1,13 @@
+nome = "Andreza"
+idade = 17
+
+
+if (idade >= 18)
+    puts nome + ", você pode tirar carteira de motorista."
+elsif (idade >= 7)
+    #verificar uma outra condição elsif
+    puts nome + ", melhor continuar andando de bicicleta."
+else 
+    puts nome + ", você é muito jovem e só pode andar de motinha"
+
+end 
