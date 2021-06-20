dfc0207fdd5e44c4b0d9f0edd655d5fed9c6e096
Andreza Silva <dedezasilva@live.com>
2021-06-20 17:13:18 -0400

    interpolação de strings

--- carteira.rb
+++ carteira.rb
@@ -7,10 +7,14 @@ idade = gets.chomp.to_i
 puts idade.class
 
 if (idade >= 18)
-    puts nome + ", você pode tirar carteira de motorista."
+    
+    # puts nome + ", você tem "+ idade.to_s + " anos e portanto pode tirar sua carteira de motorista".
+    puts "#{nome}, você tem #{idade} anos e portanto pode tirar sua carteira de motorista."
 elsif (idade >= 7)
     #verificar uma outra condição elsif
-    puts nome + ", melhor continuar andando de bicicleta."
+    # puts nome + ", você tem " + idade.to_s + " anos e portanto é melhor continuar andando de bicileta."
+    puts "#{nome}, você tem #{idade} anos e portanto é melhor continuar andando de bicicleta"
 else 
-    puts nome + ", você é muito jovem e só pode andar de motinha"
-end 
+    # puts nome + ", você tem " + idade.to_s + " anos e sendo muito jovem e só pode andar de motinha."
+    puts "#{nome}, você tem #{idade} anos e sendo muito jovem só pode andar de motinha"
+end  
