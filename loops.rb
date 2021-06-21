2dac896f05ea7e9c65e0483663b2b6cb84b7ce58
Andreza Silva <dedezasilva@live.com>
2021-06-21 13:32:03 -0400

    loops times

---
+++ loops.rb
@@ -0,0 +1,21 @@
+
+# 1ª forma 
+# 10.times do
+#     puts "Executando o times"
+# end 
+
+# 2ª forma 
+
+# i = 1
+# 10.times do 
+#     puts "Execução numero #{i}"
+#     i = i + 1
+# end 
+
+#3ª forma
+bandas = ["banda A", "banda B", "banda C", "banda D"]
+i = 0
+bandas.size.times do
+    puts bandas[i]
+    i = i + 1
+end 
